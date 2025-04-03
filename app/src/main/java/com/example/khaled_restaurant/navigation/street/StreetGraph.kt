fa8746b2fd6dd723d215f35339e50fc55e0082d5
Abdulkarim <abdyabkor@gmail.com>
2025-04-03 05:36:22 +0300

    add street screen to street graph

--- app/src/main/java/com/example/khaled_restaurant/navigation/street/StreetGraph.kt
+++ app/src/main/java/com/example/khaled_restaurant/navigation/street/StreetGraph.kt
@@ -1,17 +1,12 @@
 package com.example.khaled_restaurant.navigation.street
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.example.khaled_restaurant.navigation.StreetsGraph
+import com.example.khaled_restaurant.presentation.street.StreetScreen
 
 fun NavGraphBuilder.streetGraph(
     modifier: Modifier = Modifier,
@@ -21,13 +16,9 @@ fun NavGraphBuilder.streetGraph(
         startDestination = StreetsView
     ) {
         composable<StreetsView> {
-            Box(modifier = modifier
-                .fillMaxSize()
-                .background(Color.Black),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "from Streets", color = Color.White)
-            }
+            StreetScreen(
+                modifier = modifier
+            )
         }
 
         composable<StreetAdd> {
