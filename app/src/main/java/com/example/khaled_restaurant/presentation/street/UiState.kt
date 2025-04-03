d2fd7443694aaa59acc2179faa902935d927464f
Abdulkarim <abdyabkor@gmail.com>
2025-04-03 05:34:35 +0300

    add state for street ui

---
+++ app/src/main/java/com/example/khaled_restaurant/presentation/street/UiState.kt
@@ -0,0 +1,9 @@
+package com.example.khaled_restaurant.presentation.street
+
+import com.example.khaled_restaurant.domain.model.Street
+
+data class UiState(
+    val streets: List<Street> = emptyList(),
+    val searchName: String = "",
+    val showDialog: Boolean = false
+)
