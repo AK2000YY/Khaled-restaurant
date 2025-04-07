121af274874a9fed6dc2d75c43bc7a241a794ba0
Abdulkarim <abdyabkor@gmail.com>
2025-04-07 23:12:59 +0300

    set up UiState

--- app/src/main/java/com/example/khaled_restaurant/presentation/customer/UiState.kt
+++ app/src/main/java/com/example/khaled_restaurant/presentation/customer/UiState.kt
@@ -3,5 +3,8 @@ package com.example.khaled_restaurant.presentation.customer
 import com.example.khaled_restaurant.domain.model.Customer
 
 data class UiState(
-    val customers: List<Customer> = emptyList()
+    val customers: List<Customer> = emptyList(),
+    val showDialog: Boolean = false,
+    val searchValue: FilterType = FilterType.NameType(""),
+    val dialogType: DialogType = DialogType.Delete
 )
